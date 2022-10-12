154225580ae95af5665d51eee60bb874bfca9807
mr-z <1989429861@qq.com>
2022-10-12 18:31:36 +0800

    swagger

--- src/main/java/framework/controller/UserController.java
+++ src/main/java/framework/controller/UserController.java
@@ -4,15 +4,20 @@ import com.github.pagehelper.PageInfo;
 import framework.pojo.User;
 import framework.pojo.query.UserQuery;
 import framework.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Api(tags=  "商品控制类",value = "这是对该类一个简单描述")
+@RequestMapping("/goods")
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @ApiOperation("返回一个商品")
     @GetMapping("/userInfo")
     public PageInfo<User> userInfo(UserQuery userQuery){
         return userService.userInfo(userQuery);
--- src/main/java/framework/pojo/User.java
+++ src/main/java/framework/pojo/User.java
@@ -1,5 +1,7 @@
 package framework.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("商品实体类")
 public class User {
+    @ApiModelProperty("商品ID")
     private Integer id;
     private String name;
     private String pwd;
