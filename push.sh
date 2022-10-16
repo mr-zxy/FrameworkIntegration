2b1b7a7539f12d4f8e8ba41bbf997d133883acf7
mr-zxy <1989429861@qq.com>
2022-10-16 23:31:12 +0800

    fix：push.sh优化

--- push.sh
+++ push.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-git add .
-git commit -m "$1"
-git push origin
 
-remote=`git remote -v`
-echo "推送成功" $remote
+
+function push(){
+    git add .
+    git commit -m "$1" &>/dev/null
+    git push origin &>/dev/null
+
+    remote=`git remote -v` 
+    echo "推送成功" $remote
+}
+
+if [ $# -eq 0 ];then
+    echo "请填写commit::请输入 bash ./push.sh fix：bug"
+else push $1
+fi
 
